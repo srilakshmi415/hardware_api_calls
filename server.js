108dd57f76569fd20431a2ed8684a3726ef2a9b8
srilakshmi415 <srilakshmi@innotrat.in>
2026-08-07 18:47:09 +0530

    added new2

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const DATA_FILE = path.join(__dirname, 'data.json');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors("*"));
+app.use(cors({
+origin: "*",
+methods: ["GET", "POST", "OPTIONS"],
+allowedHeaders: ["Content-Type", "Authorization"]
+}));
 // ---- storage helpers -------------------------------------------------
 
 function readData() {
